5953288f6daa6f8254660de9d7302e6ea20ac389
Adrien Padol <adrien.padol@orange.com>
2019-02-20 17:02:10 +0100

    Added a unit test project to check unsubscription validity

---
+++ lib/FluentLayoutTests/EventSubscriptionsTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Fluent;
+using System.ComponentModel;
+namespace FluentLayoutTests
+{
+    [TestFixture()]
+    public class EventSubscriptionsTests
+    {
+        [Test()]
+        public void BasicEventHandlerTest()
+        {
+            var triggerCount = 0;
+            var label = new Label();
+            label.Text = "initial value";
+
+            label.WithLabelPropertyChangedEvent((sender, e) =>
+            {
+                triggerCount += 1;
+            });
+
+            label.Text = "Changed";
+            label.Text = "ChangedAgain";
+
+            Assert.AreEqual(2, triggerCount);
+        }
+
+        [Test()]
+        public void ClassicUnsubscribeEventHandlerTest()
+        {
+            var triggerCount = 0;
+            var label = new Label();
+            label.Text = "initial value";
+
+            PropertyChangedEventHandler handler = (sender, e) =>
+            {
+                triggerCount += 1;
+            };
+
+            label.WithLabelPropertyChangedEvent(handler);
+            label.Text = "Changed";
+
+            label.PropertyChanged -= handler;
+            label.Text = "ChangedAgain";
+
+            Assert.AreEqual(1, triggerCount);
+        }
+
+        [Test()]
+        public void FluentUnsubscribeEventHandlerTest()
+        {
+            var triggerCount = 0;
+            var label = new Label();
+            label.Text = "initial value";
+
+            PropertyChangedEventHandler handler = (sender, e) =>
+            {
+                triggerCount += 1;
+            };
+
+            label.WithLabelPropertyChangedEvent(handler);
+            label.Text = "Changed";
+
+            label.ClearLabelPropertyChangedHandlers();
+            label.Text = "ChangedAgain";
+
+            // We unsubscribed before the second Text changed
+            Assert.AreEqual(1, triggerCount);
+        }
+    }
+}
